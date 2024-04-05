25236b86954efcc246454e9b57eb0a39fd919410
Luke Mross <156094063+RabbitLuke@users.noreply.github.com>
2024-04-05 18:45:56 +0200

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiConstants {
-  static String get baseUrl => 'http://10.0.1.7:8080';
+  static String get baseUrl => 'Address';
   static const String contentType = 'application/json';
   static const String accessTokenKey = 'accessToken';
   static const String refreshTokenKey = 'refreshToken';
