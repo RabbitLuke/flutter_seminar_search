8ae0b15679c76c026fd33afe929b567d5a01c7fa
RabbitLuke <LMross@retrorabbit.co.za>
2024-04-05 16:21:27 +0200

    small change

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiConstants {
-  static String get baseUrl => 'ipAddressHere';
+  static String get baseUrl => 'Nice try pal';
   static const String contentType = 'application/json';
   static const String accessTokenKey = 'accessToken';
   static const String refreshTokenKey = 'refreshToken';
