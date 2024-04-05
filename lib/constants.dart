be5d4a78d8b577da628a597630ea2202d0373a75
RabbitLuke <LMross@retrorabbit.co.za>
2024-04-05 20:37:11 +0200

    Brought back from the dead

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiConstants {
-  static String get baseUrl => 'Nice try pal';
+  static String get baseUrl => 'http://10.0.3.6:8080';
   static const String contentType = 'application/json';
   static const String accessTokenKey = 'accessToken';
   static const String refreshTokenKey = 'refreshToken';
